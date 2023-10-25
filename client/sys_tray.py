2280c66e33c7044111950cc931163b7ee9683c04
z@d.com <z@d.com>
2023-10-25 17:46:52 +0530

    Removed tray, minimize instead

--- client/sys_tray.py
+++ client/sys_tray.py
@@ -1,47 +1,9 @@
 import os
-import pystray
-from PIL import Image
-import sys
 
-try:
-    BASE_PATH = sys._MEIPASS
-except AttributeError:
-    BASE_PATH = os.path.dirname(__file__)
-
-image = Image.open(BASE_PATH+'/views/favicon.ico')
-
-click_callback = None
-
-
-def click(icon, item):
-    print('[TRAY CALLBACK]---')
-    print(click_callback() if click_callback else 'No callback registered')
-    print('[TRAY CALLBACK]---')
-
-def start(block=False):
-    icon.run() if block else icon.run_detached()
-
-
-def stop():
-    print('Killing tray')
-    os.system(f'kill -9 {os.getpid()}')
-    icon.stop()
-
+ICON_PATH = 'anydesk'
 
 def notify(text):
-    os.system(
-        f'notify-send -i \'{BASE_PATH+"/views/favicon.ico"}\' "TheChat" "{text}"')
-
-
-icon = pystray.Icon("TC", image, "TheChat", menu=pystray.Menu(
-    pystray.MenuItem("Open", click,default=True),
-    pystray.MenuItem("Quit", stop),
-))
-
+    os.system(f'notify-send -i {ICON_PATH} "TheChat - Alert" "{text}"')
 
 if __name__ == '__main__':
-    try:
-        print(icon)
-        start(True)
-    except KeyboardInterrupt:
-        stop()
+    notify("test")
